2b9261e78eadebf417654a50d49cde63f86e45e8
ultronDebugs <muftahuddeenabba@gmail.com>
2024-09-13 21:26:44 +0100

    created login/signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
-        <div className="px-6">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<h2>signup</h2>} />
-            <Route path="/login" element={<h2>login page</h2>} />
-            <Route path="/:id" element={<h1>sorry page not found</h1>} />
-          </Routes>
+        <div className="px-6 md:max-h-[900px] sm:max-h-[900px] lg:max-h-[800px] overflow-y-scroll  ">
+          <div className="flex items-center">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/:id" element={<h1>sorry page not found</h1>} />
+            </Routes>
+          </div>
+          <SignupPage />
+          <SignupPage />
+          <SignupPage />
+          <SignupPage />
+          <SignupPage />
+          <SignupPage />
+          <SignupPage />
         </div>
       </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Navbar() {
   return (
-    <div className="flex px-6 justify-between items-center">
+    <div className="flex px-6 justify-between py-2 items-center">
       <Link to={"/"}>
         <img src="https://ekbkhbre" alt="logo" />
       </Link>
-      <div className="flex xl:gap-8 sm:gap-2">
+      <div className="flex items-center xl:gap-8 sm:gap-2">
         <Link to={"/about"}>About</Link>
         <Link to={"/contact"}> contact</Link>
         <Link to={"/cart"}>cart</Link>
         <Link to={"/login"}>login</Link>
         <Link to={"/signup"}>singup</Link>
-        <Link to={"/profile"}>Profile</Link>
+        <Link to={"/profile"}>
+          <Avatar className="h-12 w-12">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Link>
       </div>
     </div>
   );
